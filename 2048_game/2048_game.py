7fdc379dd6c266a7356ead4c8bf4ba39ddda8485
maihw <704687027@qq.com>
2020-09-03 16:31:02 +0800

    2048_game

---
+++ 2048_game/2048_game.py
@@ -0,0 +1,7 @@
+import curses
+from random import randrange, choice
+from collections import defaultdict
+actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
+letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
+actions_dict = dict(zip(letter_codes, actions * 2))
+
